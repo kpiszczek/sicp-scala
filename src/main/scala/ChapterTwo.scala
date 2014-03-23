d1e2bc09188369fe3f75f10ea307d08aecf4d912
Kuba Piszczek <jppiszczek@gmail.com>
2014-03-23 17:31:42 +0000

    ex 2.2, 2.3

--- src/main/scala/ChapterTwo.scala
+++ src/main/scala/ChapterTwo.scala
@@ -1,6 +1,7 @@
 package kuba.sicp
 
 import scala.language.implicitConversions
+import scala.math.sqrt
 
 class Rational(n: Int, d: Int) {
   import ChapterOne.gcd
@@ -41,6 +42,22 @@ object Rational {
   def apply(n: Int) = new Rational(n, 1)
   def unapply(rational: Rational): Option[(Int, Int)] = 
     Some((rational.numer, rational.denom))
+}
+
+// ex 2.2
+case class Point(x: Double, y: Double)
+case class Segment(start: Point, end: Point) {
+  lazy val midpoint = Point((start.x + end.x) / 2.0, (start.y + end.y) / 2.0)
+}
+// ex.2.3
+case class Rectangle(val a: Point, val b: Point) {
+  import ChapterOne.abs
+  lazy val area = ((b.x - a.x) * (b.y - a.y)).abs
+  lazy val perimeter = abs(2.0 * (b.x - a.x)) + abs(2.0 * (b.y - a.y))
+}
+object Rectangle {
+  def apply(topLeft: Point, width: Double, height: Double): Rectangle = 
+    Rectangle(topLeft, Point(topLeft.x + width, topLeft.y - height))
 }
 
 object ChapterTwo {
