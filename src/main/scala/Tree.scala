8d8c7ee33b293af23d1975eb25629d203a3ec4c6
Kuba Piszczek <jppiszczek@gmail.com>
2014-04-03 20:44:25 +0000

    ex 2.27

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -2,12 +2,16 @@ package kuba.sicp
 
 import scala.language.implicitConversions
 
+// ex 2.27
 abstract class Tree[A] {
   def countLeaves: Int
+  def deepReverse: Tree[A]
 }
 case class Branch[A](nodes: Tree[A]*) extends Tree[A] {
   def countLeaves = nodes.foldLeft(0)(_ + _.countLeaves)
+  def deepReverse = Branch[A](nodes.toList.reverse.map(_.deepReverse):_*)
 }
 case class Leaf[A](value: A) extends Tree[A] {
   def countLeaves = 1
+  def deepReverse = this
 }
