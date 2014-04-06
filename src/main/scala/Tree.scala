0fa3bdee27e6e9927fdc973e3e856fbbb7eb8fb2
Kuba Piszczek <jppiszczek@gmail.com>
2014-04-06 12:21:53 +0000

    ex 2.30, 2.31

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -2,19 +2,23 @@ package kuba.sicp
 
 import scala.annotation.tailrec
 import scala.language.implicitConversions
+import Numeric._
 
-// ex 2.27, 2.28
+// ex 2.27, 2.28, 2.30, 2.31
 abstract class Tree[A] {
   def countLeaves: Int
   def deepReverse: Tree[A]
+  def map[B](fn: A => B): Tree[B]
 }
 case class Branch[A](nodes: Tree[A]*) extends Tree[A] {
   def countLeaves = nodes.foldLeft(0)(_ + _.countLeaves)
   def deepReverse = Branch[A](nodes.reverse.map(_.deepReverse):_*)
+  def map[B](fn: A => B) = Branch[B](nodes.toList.map(x => x.map[B](fn)):_*)
 }
 case class Leaf[A](value: A) extends Tree[A] {
   def countLeaves = 1
   def deepReverse = this
+  def map[B](fn: A => B) = Leaf[B](fn(value))
 }
 
 object Tree {
@@ -30,4 +34,8 @@ object Tree {
       Branch(go(nodes.toList, Nil):_*)
     }
   }
+  def scaleTree[A](tree: Tree[A], factor: A)(implicit num: Numeric[A]): Tree[A] = 
+    tree.map(value => num.times(factor, value))
+  def squareTree[A](tree: Tree[A])(implicit num: Numeric[A]): Tree[A] =
+    tree.map(value => num.times(value, value))
 }
